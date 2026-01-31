92ab03464d6a1df4c2eeb4803611bac584c87194
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-01-31 10:45:53 -0300

    Mapeando rota POST

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,19 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    // Metodo
+    @PostMapping
+    public void cadastrar() {
+
+    }
+
+
+}
