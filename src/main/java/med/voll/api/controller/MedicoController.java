3ca5dd302c5926c0c2ff2beca6c2f69b53d4287e
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-03 19:14:20 -0300

    Injeção de dependencia para o spring comandar o repository

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,16 +1,24 @@
 package med.voll.api.controller;
 
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    //Atributos
+    @Autowired
+    private MedicoRepository repository;
+
     // Metodo
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+//        System.out.println(dados);
+
+
     }
 
 
