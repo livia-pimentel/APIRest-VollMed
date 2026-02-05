520f6e2773c38e75347e4548556755ed75c71b71
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-04 21:39:40 -0300

    Add funcinalidade de atualizar pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,10 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.paciente.DadosCadastroPaciente;
-import med.voll.api.paciente.DadosListagemPaciente;
-import med.voll.api.paciente.Paciente;
-import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -32,6 +29,14 @@ public class PacienteController {
     public Page<DadosListagemPaciente> listarPaciente(@PageableDefault(size = 10, sort = {"nome"})Pageable paginacao) {
         return pacienteRepository.findAll(paginacao)
                 .map(DadosListagemPaciente::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizarPaciente(@RequestBody @Valid DadosAtualizarPaciente dados) {
+        var paciente = pacienteRepository.getReferenceById(dados.id());
+
+        paciente.atualizarInformacoes(dados);
     }
 
 }
