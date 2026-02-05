98c9130049f20e9ca0b665480efc0ed3f8fc3d61
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-04 21:27:15 -0300

    Add funcionalidade de atualizar medico

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,6 +1,7 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -38,5 +39,21 @@ public class Medico {
         this.crm = dados.crm();
         this.endereco = new Endereco(dados.endereco());
         this.especialidade = dados.especialidade();
+    }
+
+    public void atualizarInformacoes(@Valid DadosAtualizacaoMedico dados) {
+
+        // Verifica se a informação está vindo no DTO, pois não são campos obrigatorios
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
     }
 }
