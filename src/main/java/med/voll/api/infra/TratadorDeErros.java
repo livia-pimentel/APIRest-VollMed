4bc2f679cdc213be34c28ff2130027ccc2d1d3a7
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-05 15:28:13 -0300

    Tratando erros com anotação spring

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -1,4 +1,17 @@
 package med.voll.api.infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.hibernate.action.internal.EntityActionVetoException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class TratadorDeErros {
+
+    // Metodo
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
 }
