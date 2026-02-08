4bbe0ca92e0a9147e8cf24bda82f28a664ab578d
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-08 18:21:47 -0300

    Add filtro nas requisições

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,21 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+// Essa classe faz o filtro apenas 1 vez por requisição
+public class SecurityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // Chama o próximo filtro
+        filterChain.doFilter(request, response);
+    }
+}
