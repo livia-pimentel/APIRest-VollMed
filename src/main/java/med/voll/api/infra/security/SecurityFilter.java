e58f8cf46dd5a46a14215ceddd2359c07686f542
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-09 08:16:50 -0300

    Add metodo para recuperar o token JWT

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -15,7 +15,23 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        var tokenJWT = recuperarToken(request);
+
+        System.out.println(tokenJWT);
+
         // Chama o próximo filtro
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+
+        var authorizationHeader = request.getHeader("Authorization");
+        // Verificar se está vindo o token no cabeçalho
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+
+        // Retorna o token sem o prefixo (Bearer)
+        return authorizationHeader.replace("Bearer ", "");
     }
 }
