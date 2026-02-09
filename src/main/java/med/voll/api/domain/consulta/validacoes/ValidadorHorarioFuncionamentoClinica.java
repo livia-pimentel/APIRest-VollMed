2f9ebf1f7063da1e1d15a878299185c93881e33f
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-09 16:19:00 -0300

    Verificando dia e horario de funcionamento da clinica

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,22 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+
+public class ValidadorHorarioFuncionamentoClinica {
+
+    // Metodo
+    public void validar(DadosAgendamentoConsulta dados) {
+
+        var dataConsulta = dados.data();
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDaAberturaDaClinica = dataConsulta.getHour() < 7;
+        var depoisDoFechamentoDaClinica = dataConsulta.getHour() > 18;
+
+        if (domingo || antesDaAberturaDaClinica || depoisDoFechamentoDaClinica) {
+            throw new ValidacaoException("Consulta do horário de funcionamento da clínica.");
+        }
+    }
+}
