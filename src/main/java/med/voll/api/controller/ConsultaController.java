fafb49a0ca0d8f795cfa1018045123e19dd9006a
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-09 10:29:16 -0300

    Add DTO(usando Record) que devolverá o detalhamento da consulta

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,6 @@ public class ConsultaController {
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
         System.out.println(dados);
-        return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null));
+        return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
 }
