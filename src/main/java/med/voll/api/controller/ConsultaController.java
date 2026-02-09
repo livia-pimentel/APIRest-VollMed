4bdf548fb55976e237d36f8456c457f56fa58c70
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-09 10:24:48 -0300

    Add DTO(usando Record) para dados que chega da API

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
