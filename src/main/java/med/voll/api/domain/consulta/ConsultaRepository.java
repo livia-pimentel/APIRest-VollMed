2d294af9b451f2ada20bed1c997d8d249ac9d375
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-09 18:41:48 -0300

    Verificação se paciente já tem uma consulta no dia

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -8,5 +8,7 @@ import java.time.LocalDateTime;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
-    Boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
+    boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
+
+    boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 }
