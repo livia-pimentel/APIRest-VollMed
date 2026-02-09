2bfeffae90e4e5ddc3aa9403f8e5240158cdf693
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-09 19:03:31 -0300

    Add anotações do spring nos validadores

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -2,9 +2,11 @@ package med.voll.api.domain.consulta.validacoes;
 
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.stereotype.Component;
 
 import java.time.DayOfWeek;
 
+@Component
 public class ValidadorHorarioFuncionamentoClinica implements ValidadorAgendamentoDeConsulta{
 
     // Metodo
--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
@@ -3,10 +3,14 @@ package med.voll.api.domain.consulta.validacoes;
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ValidadorMedicoAtivo implements ValidadorAgendamentoDeConsulta{
 
     // Atributo
+    @Autowired
     private MedicoRepository repository;
 
     // Metodo
