cf2611ef33d671faee26a3bb27b8d852e4216706
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-09 18:00:23 -0300

    Verificação se médico está ativo

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
@@ -0,0 +1,26 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.medico.MedicoRepository;
+
+public class ValidadorMedicoAtivo {
+
+    // Atributo
+    private MedicoRepository repository;
+
+    // Metodo
+    public void validar(DadosAgendamentoConsulta dados) {
+        //Escolha do médico opcional
+        if (dados.idMedico() == null) {
+            return;
+        }
+
+        var medicoEstaAtivo = repository.findAtivoById(dados.idMedico());
+        if (!medicoEstaAtivo) {
+            throw new ValidacaoException("Consulta não pode ser agendada com médico excluído");
+        }
+    }
+
+
+}
