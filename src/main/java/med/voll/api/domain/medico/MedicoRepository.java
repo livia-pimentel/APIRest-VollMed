27704e7d896f3e5ff7e6e4df12234ca4bf6bf527
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-10 07:41:03 -0300

    Correção no do parametro na query

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -35,6 +35,6 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             where
             m.id = :id
             """)
-    Boolean findAtivoById(Long idMedico);
+    Boolean findAtivoById(Long id);
 
 }
